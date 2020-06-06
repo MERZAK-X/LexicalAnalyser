0298e2118e9b06981c1dc69b9d03542eb86b2e15
Mohamed MERZAK <MERZAK_MOHAMED@emsi-edu.ma>
2020-06-06 18:02:56 +0100

    Fixed #6

--- src/Entities/DeterministicFiniteStateAutomaton.cs
+++ src/Entities/DeterministicFiniteStateAutomaton.cs
@@ -16,7 +16,9 @@ namespace LexicalAnalyzer.Entities
         private List<int> _states = new List<int>();
         private List<string> _keywords;
         private string _alphabet, _name;
-        private bool comment = false; // Checks whether the input is a comment
+        private string[] _commentDelimiter;
+        private string _string = string.Empty; 
+        private bool _isText = false, _comment = false; // Checks whether the input is a string / comment
         private TransitionsMap _transitions = new TransitionsMap();
 
         #endregion
@@ -40,7 +42,8 @@ namespace LexicalAnalyzer.Entities
             _finalStates = Array.ConvertAll(finalStatesAsString, int.Parse);
             _keywords = lines[5].Split(' ').ToList();
             _keywords = _keywords.ConvertAll(keyword => keyword.ToLower());
-            for (var i = 6; i <= lines.Length - 1; i++)
+            _commentDelimiter = lines[6].Split(' ').ToArray();
+            for (var i = 7; i <= lines.Length - 1; i++)
             {
                 if(lines[i].StartsWith('#')) continue; // Adds #3
                 var state = int.Parse(lines[i].Split(' ')[0]);
@@ -130,36 +133,83 @@ namespace LexicalAnalyzer.Entities
 
         public bool Analyse(string sourceCode)
         {
+            #region Variables
             var accepted = true; var currentAccepted = false;
             char [] separators = {' ', '\n', '\t', ';'};
             var tokenId = 0; TokensMap token;
+            #endregion
+            
             foreach (var word in sourceCode.Split(separators))
             {
                 tokenId = Accepts(word); // Get the final state; 0 = none = word not accepted
-                if (tokenId == -1) continue;
-                tokenId = (tokenId == 5 || tokenId == 9) ? 10 // Checks if a `REL OP`, since enums cannot take multi-values
-                    : (tokenId == 2) ? 11 // Checks if a `ARTH OP`, since enums cannot take multi-values
-                    : (tokenId == 3) ? 8 // Checks if a `REAL`, since enums cannot take multi-values
-                    : tokenId;
+
+                #region Check for Tokens that could not be described in TockensMap Enum
+                switch (tokenId) {
+                    case -1: case -2: // Comment occured
+                        continue;
+                    case 2: // Checks if a `ARTH OP`, since enums cannot take multi-values
+                        tokenId = 11; break;
+                    case 3: // Checks if a `REAL`, since enums cannot take multi-values
+                        tokenId = 8; break;
+                    case 5: case 9: // Checks if a `REL OP`, since enums cannot take multi-values
+                        tokenId = 10; break;
+                    case 13: // Checks for string and aggregates it 
+                        _string += ' '+word+' '; break;
+                }
+                #endregion
+                
                 currentAccepted = (tokenId != 0); // if a final state was found
                 token = (TokensMap) tokenId; // Get token by id from the map
                 accepted = accepted && currentAccepted; // If the previous words and current one are accepted -> for the last return statement
-                Console.WriteLine($@"{((currentAccepted) ? '\u2713' : '\u2717')} <{word}{((token != 0) ? ","+token : string.Empty )}>");
+                Console.WriteLine($@"{((currentAccepted) ? '\u2713' : '\u2717')} <{word}{((token != 0) ? ", "+token : string.Empty )}>");
             }
+            
             return accepted;
         }
 
         public int Accepts(string word)
         {
+            #region Variables
             var letters = (word.ToLower()).ToCharArray();
             var state = _startState;
-            if (!comment) {comment = word.StartsWith("(*");}
-            if (comment) {comment = !word.EndsWith("*)"); return -1;}
+            #endregion
+
+            #region Check for empty word string.Empty
+            if (word == string.Empty) return -2; // Check for empty word '' caused by spaces in source code
+            #endregion
+
+            #region Check for comments
+            if (!_comment)
+                _comment = word.StartsWith(_commentDelimiter[0]);
+            
+            if (_comment){
+                _comment = !word.EndsWith(_commentDelimiter[1]);
+                return -1;
+            }
+            #endregion
+
+            #region Check for strings
+            if (!_isText)
+                _isText = word.StartsWith('"');
+            
+            if (_isText){
+                _isText = !word.EndsWith('"');
+                return 13;
+            }
+            #endregion
+
+            #region Check for Keywords
             if (_keywords.Any(x=> x == word)) return 12; // Checks whether the word is a keyword from the _keywords array
+            #endregion
+
+            #region Calculate the Final State
+
             foreach (var letter in letters)
                 try{ state = _transitions[state, letter]; /*Console.WriteLine(letter); // Uncomment for debug */} 
                 catch (NullReferenceException) { return 0; }
                 catch (Exception) { return 0; }
+
+            #endregion
 
             return ((IList) this._finalStates).Contains(state) ? state : 0; // Return the final state instead of a bool ; 0 = no states found
         }
