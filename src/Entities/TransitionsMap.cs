9f9933cff9bb2e7e8d1ed1b9395c2dae4775fd43
Mohamed MERZAK <MERZAK_MOHAMED@emsi-edu.ma>
2020-06-07 19:43:46 +0100

    Update TransitionsMap.cs

--- src/Entities/TransitionsMap.cs
+++ src/Entities/TransitionsMap.cs
@@ -5,10 +5,8 @@ namespace LexicalAnalyzer.Entities
 {
     public class TransitionsMap : List<Transition>
     {
-        public int Σ(int state, char alpha) => this[state].σ(state, alpha); // Returns the final state of the transition
-        
         public override string ToString() => this.Aggregate("", (current, transition) => current + $" [ {transition} ] ");
 
-        public int this[int i, char c] => Find(transition => transition.isΣ(i,c)).σ(i,c);
+        public int this[int i, char c] => Find(transition => transition.isΣ(i,c)).σ(i,c); // Returns the final state of the transition
     }
 }
