01b0e9a14f1c1a038d07d58a19c4ba3c50c21181
Mohamed MERZAK <MERZAK_MOHAMED@emsi-edu.ma>
2020-06-07 19:47:45 +0100

    Update Token.cs

--- src/Entities/Token.cs
+++ src/Entities/Token.cs
@@ -4,11 +4,11 @@ namespace LexicalAnalyzer.Entities
     {
         ID      = 1,
         KEYWORD = 12,
-        ARTH_OP = 11,
-        REL_OP  = 10,
+        ARTH_OP = 11, // & 2
+        REL_OP  = 10, // & 5 & 9
         STRING  = 13,
         INT     = 4,
-        REAL    = 8,
+        REAL    = 8, // & 3
         COMMENT = 14
     }
 }
