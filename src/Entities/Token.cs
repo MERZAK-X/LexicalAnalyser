64b0a697f6f4ff4de944807091bace916ee7f3dc
Mohamed MERZAK <MERZAK_MOHAMED@emsi-edu.ma>
2020-06-10 12:15:05 +0100

    Fixed issue #10 for single OP type per word

--- src/Entities/Token.cs
+++ src/Entities/Token.cs
@@ -8,6 +8,18 @@ namespace LexicalAnalyzer.Entities
 
         public Token(int tokenId, string value)
         {
+            #region Check for Tokens that could not be described in TokenTypes Enum
+            switch (tokenId) {
+                case -1: case -2: // Comment or string.Empty occured 
+                    tokenId = 14; break;
+                case 2: // Checks if a `ARTH OP`, since enums cannot take multi-values 
+                    tokenId = 11; break;
+                case 3: // Checks if a `REAL`, since enums cannot take multi-values 
+                    tokenId = 8; break;
+                case 5: case 9: // Checks if a `REL OP`, since enums cannot take multi-values 
+                    tokenId = 10; break;
+            }
+            #endregion
             _tokenId = (TokenType) tokenId;
             _value = value;
             _accepted = (_tokenId != 0); // True if the token has an accepted final state != 0
