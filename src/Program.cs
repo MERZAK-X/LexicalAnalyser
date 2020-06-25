6f5ed174c49f57bc174a726803155ec1194e90ca
Mohamed MERZAK <MERZAK_MOHAMED@emsi-edu.ma>
2020-06-25 17:37:25 +0100

    Added Quiet option

--- src/Program.cs
+++ src/Program.cs
@@ -9,15 +9,16 @@ namespace LexicalAnalyser
     {
         #region Help message
         private const string Help = 
-@"LexicalAnalyser v1.3: https://github.com/MERZAK-X/LexicalAnalyser
+@"LexicalAnalyser v1.4: https://github.com/MERZAK-X/LexicalAnalyser
 
-Usage: dotnet LexicalAnalyser.dll [[Automaton] [Sourcecode]] [-v] [--help]
+Usage: dotnet LexicalAnalyser.dll [[Automaton] [Sourcecode]] [-v|-vv|-q] [--help]
 
     Arguments:
         Automaton       Path to the Automaton's file
         Source          Path to the source code file to be analysed
     Options:
         -v, -vv         Verbose level, 1 or 2 respectively, if not set 0
+        -q              Quiet (verbose level -1), only display results
         --help          Display this help and exit
         
 Examples:
@@ -36,11 +37,11 @@ Copyright (C) 2020 ""NUL-X""";
                 if (args.Length >= 2 && !args.Contains("--help"))
                 { 
                     // Set the verbose level
-                    var verbose = (args.Contains("-v")) ? 1 : (args.Contains("-vv")) ? 2 : 0;
+                    var verbose = (args.Contains("-q")) ? 0 : (args.Contains("-v")) ? 1 : (args.Contains("-vv")) ? 2 : 0;
                     // Create new Deterministic Finite State Automaton from given file
                     var automaton = DFSA.CreateInstance(args[0] ?? Console.ReadLine(), verbose);
-                    // Display Automaton
-                    automaton.Print();
+                    // Display Automaton if not set to quiet
+                    if (!args.Contains("-q")) automaton.Print();
                     // Analyse the given source code & display the results
                     var accepted = automaton.Analyse(File.ReadAllText(args[1] ?? Console.ReadLine()));
                     Console.WriteLine($@"{'\n'}{((accepted) ? '\u2713' : '\u2717')} The source file `{Path.GetFileName(args[1])}` is {((accepted) ? string.Empty : "NOT " )}accepted by the automaton's described language !");
